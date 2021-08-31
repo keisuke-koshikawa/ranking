949d1e3f060b3009acf35f34c00e31533827a3d3
kkoshikawa <48783545+kkoshikawa@users.noreply.github.com>
2021-08-31 22:57:04 +0900

    Update companies_controller.rb

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,9 +1,5 @@
 class CompaniesController < ApplicationController
-    def index
-        @companies = Company.find(params[:id])
-    end
-
-    def show
-        @companies = Company.find(params[:id])
-    end
+  def show
+    @companies = Company.find(params[:id])
+  end
 end
